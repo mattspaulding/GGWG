4b0b18e6b4454d66f8ab9219763a64d319fe4ed6
Matt Spaulding <mrmattspaulding@gmail.com>
2016-09-04 16:02:42 -0400

    Added bridge.

---
+++ Assets/GGWG/Game/Levels/GGWG Demo/Scripts/OpenBridge.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OpenBridge : MonoBehaviour
+{
+
+    private Player player;
+    public GameObject door;
+    public float moveDistance = 5f;
+    public float moveVelocity = 5f;
+    private bool move;
+    private float originalX;
+
+    // Use this for initialization
+    void Start()
+    {
+        player = FindObjectOfType<Player>();
+        originalX = door.transform.position.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(move)
+        door.transform.position = Vector2.MoveTowards(door.transform.position, new Vector2(originalX + moveDistance,door.transform.position.y), moveVelocity * Time.deltaTime);
+        if (door.transform.position.x == originalX + moveDistance)
+            move = false;
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+                move = true;
+       
+        }
+    }
+}
